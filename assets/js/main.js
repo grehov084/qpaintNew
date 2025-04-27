29bb8223cad1e1610708a5d54dae72427a22a53c
grehov084 <grehov084@gmail.com>
2025-04-27 23:23:54 +0700

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
             navText: ['<svg width="31" height="10" viewBox="0 0 31 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M31.001 5H0.999023M0.999023 5L5.00137 1M0.999023 5L5.00137 9" stroke="white"/></svg>', '<svg width="32" height="16" viewBox="0 0 32 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.999023 8H31.001M31.001 8L26.9986 4M31.001 8L26.9986 12" stroke="white"/></svg>'],
             navClass: ['slider-arrows__item slider-arrows-item slider-arrows-item--left', 'slider-arrows__item slider-arrows-item slider-arrows-item--right'],
             dotClass: "slider-nav-dots__item",
+            itemClass: "slider-item",
         });
     });
 
